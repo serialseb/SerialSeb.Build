d72b58e1c9a39f2e32cf646df4d0bf0fa6d0b7be
Sebastien Lambla <seb@serialseb.com>
2017-03-10 10:26:33 +0100

    I still don't know how to use here-strings

--- SerialSeb.Build/Install.ps1
+++ SerialSeb.Build/Install.ps1
@@ -39,7 +39,8 @@ $ $env:SSB_FUNCS/Install-Tools.ps1
 Add-AppVeyorMessage "Install - Tools installed from nuget" -Details @"
 $env:SSB_XUNIT_PATH
 $env:SSB_OPENCOVER_PATH
-$env:SSB_COVERALLS_PATH"@
+$env:SSB_COVERALLS_PATH
+"@
 
 
 & $env:SSB_FUNCS/Set-Version.ps1
